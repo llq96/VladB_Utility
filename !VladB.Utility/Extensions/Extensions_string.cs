2f37e8dc908ba237def67d8ba7281fa80a7331c5
VladPC <bovinov.vlad@gmail.com>
2021-08-18 00:41:12 +0300

    v0.14.1
    Remove Folder
    Add .gitignore

--- !VladB.Utility/Extensions/Extensions_string.cs
+++
@@ -1,7 +0,0 @@
-namespace VladB.Utility {
-    public static partial class Extensions {
-        public static bool IsNullOrEmpty(this string s) => string.IsNullOrEmpty(s);
-        public static bool IsHaveSomething(this string s) => !string.IsNullOrEmpty(s);
-    }
-}
-
